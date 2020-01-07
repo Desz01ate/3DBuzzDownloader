b08b7c6c046de3871f95252701b50cd107524015
Deszolate_C <deszolate@icloud.com>
2020-01-07 14:13:43 +0700

    Add project files.

---
+++ 3DBuzzDownloader/HtmlHelper/Source.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace _3DBuzzDownloader.HtmlHelper
+{
+    public static class Source
+    {
+        public static async IAsyncEnumerable<string> GetSourceAsync(string url)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            var response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var receiveStream = response.GetResponseStream();
+                StreamReader readStream;
+                if (string.IsNullOrWhiteSpace(response.CharacterSet))
+                    readStream = new StreamReader(receiveStream);
+                else
+                    readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+
+                while (!readStream.EndOfStream)
+                {
+                    var currentLine = await readStream.ReadLineAsync();
+                    yield return currentLine;
+                }
+
+                response.Close();
+                readStream.Close();
+            }
+        }
+        public static string GetSource(string url)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            var response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var receiveStream = response.GetResponseStream();
+                StreamReader readStream;
+                if (string.IsNullOrWhiteSpace(response.CharacterSet))
+                    readStream = new StreamReader(receiveStream);
+                else
+                    readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+
+                var data = readStream.ReadToEnd();
+                response.Close();
+                readStream.Close();
+                return data;
+            }
+            return null;
+        }
+    }
+}
---
+++ 3DBuzzDownloader/Program.cs
@@ -0,0 +1,62 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace _3DBuzzDownloader
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //await foreach (var line in HtmlHelper.Source.GetSourceAsync("https://www.3dbuzz.com/"))
+            //{
+            //    Console.WriteLine(line);
+            //}
+            //return;
+            var htmlSource = HtmlHelper.Source.GetSource("https://www.3dbuzz.com");
+            if (htmlSource == null)
+            {
+                htmlSource = File.ReadAllText("Resources\\source.html");
+            }
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(htmlSource);
+            var availableFiles = htmlDoc.DocumentNode.Descendants("a").Select(x => x.Attributes["href"].Value);
+            Console.WriteLine($"Found {availableFiles.Count()} files.");
+            Console.Write($"Please enter download destination : ");
+            var baseDir = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(baseDir) && Directory.Exists(baseDir))
+            {
+                foreach (var file in availableFiles)
+                {
+                    var fileName = file.Substring(file.LastIndexOf("/") + 1);
+                    var savedFile = Path.Combine(baseDir, fileName);
+                    var wc = Download(file, savedFile);
+                    wc.DownloadProgressChanged += (s, e) =>
+                    {
+                        Console.Write($"\r--Downloading {fileName}... {e.ProgressPercentage}%");
+                    };
+                    while (wc.IsBusy)
+                    {
+                        await Task.Delay(1000);
+                    }
+                    Console.WriteLine(" Done.");
+                    //Console.Write($@"--Downloading {fileName}... ");
+                    //Console.WriteLine("Done");
+                }
+            }
+
+        }
+
+
+        public static WebClient Download(string fileUri, string savePath)
+        {
+            using var client = new WebClient();
+            var uri = new Uri(fileUri);
+            client.DownloadFileAsync(uri, savePath);
+            return client;
+        }
+    }
+}
